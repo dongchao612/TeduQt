e40e8914f88c015be9ea35b7a1d9fd3b76fa81ed
dongchao980612 <dongchao980612@163.com>
2024-12-28 18:52:23 +0800

    add button demo

--- Day01/02_button/main.cpp
+++ Day01/02_button/main.cpp
@@ -7,15 +7,15 @@ int main(int argc,char** argv)
 {
 	QApplication app(argc,argv);
 	
-	//创建编码对象
-    // QTextCodec * coder=QTextCodec::codecForName("gbk");
+	// 创建编码对象
+	QTextCodec * coder=QTextCodec::codecForName("gbk");
 	 
-	//将要显示的gbk编码字符串转换为unicode
-    QLabel label("我是标签!");
+	// 将要显示的gbk编码字符串转换为unicode
+    	QLabel label(coder->toUnicode("我是标签!"));
 
 	label.show();
 	
-    QPushButton button("我是按钮!");
+    	QPushButton button(coder->toUnicode("我是按钮!"));
 	
 	button.show();
 
